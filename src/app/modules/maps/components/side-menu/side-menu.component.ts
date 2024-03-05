92598eb6c0732721a0ef152f1663bd2fe16c0812
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-05 11:26:42 +0100

    feat(side-menu): add structure and functionality to side-menu navegate

--- src/app/modules/maps/components/side-menu/side-menu.component.ts
+++ src/app/modules/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'src/app/interfaces/menuItem';
 
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent {
+
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'FullScreen' },
+    { route: '/maps/zoom-range', name: 'ZoomRange' },
+    { route: '/maps/markers', name: 'Markers' },
+    { route: '/maps/properties', name: 'Houses' },
+  ]
 
 }
